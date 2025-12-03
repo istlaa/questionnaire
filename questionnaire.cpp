e1fe2492cc3270928c54f04d31ed2a61fdf3ea8a
MouhamedB19 <132092790+MouhamedB19@users.noreply.github.com>
2025-12-03 09:31:11 +0100

    ajout de la classe questionnaireSecondeChance et implémentation du score des questionnaires (et bien plus encore je crois)

--- questionnaire.cpp
+++ questionnaire.cpp
@@ -1,4 +1,49 @@
 #include"questionnaire.h"
 
-questionnaire::questionnaire(const std::vector<unique_ptr<question>>&questions):d_questions{{}}
+questionnaire::questionnaire(const std::vector<unique_ptr<question>>&questions,const string &titre):d_questions{{}},d_score{0},d_titre{titre}
+{}
+
+vector<unique_ptr<question>> questionnaire::questionsFausses()  const
+{
+    vector<unique_ptr<question>> qFausses{};
+    for(const auto &q:d_questions)
+    {
+        if(q->etat() == REPONDUFAUX)
+            qFausses.push_back(move(q));
+    }
+    return qFausses;
+}
+
+int questionnaire::score()  const
+{
+    return d_score;
+}
+
+int questionnaire::nbQuestionsRepondus()    const
+{
+    int nb = 0;
+    for(const auto &q:d_questions)
+    {
+        if(q->etat() != NONREPONDU)
+            nb++;
+    }
+    return nb;
+}
+
+string questionnaire::titre()   const
+{
+    return d_titre;
+}
+
+bool questionnaire::estFini()   const
+{
+    return nbQuestionsRepondus() == d_questions.size();
+}
+
+void questionnaire::changeScore(int nscore)
+{
+    d_score = nscore;
+}
+
+questionnaireSecondeChance::questionnaireSecondeChance():questionnaire{questionsFausses(),titre()} //est-ce que j'ai le droit de faire ça ???
 {}
--- questionnaire.h
+++ questionnaire.h
@@ -7,11 +7,24 @@ using namespace std;
 
 class questionnaire {
     public:
-        questionnaire(const std::vector<unique_ptr<question>>&questions);
+        questionnaire(const vector<unique_ptr<question>>&questions,const string &titre);
+        vector<unique_ptr<question>> questionsFausses() const;
+        int nbQuestionsRepondus()   const;
+        string titre()  const;
+        bool estFini() const;
+        int score() const;
+        void changeScore(int nscore); 
     private:
         string d_titre;
-        std::vector<unique_ptr<question>> d_questions;
+        int d_score;
+        vector<unique_ptr<question>> d_questions;
 
+};
+
+
+class questionnaireSecondeChance:public questionnaire{
+    public:
+        questionnaireSecondeChance();
 };
 
 #endif

735103d0e152467e054eaabdddcc304915d0eaad
MouhamedB19 <132092790+MouhamedB19@users.noreply.github.com>
2025-12-10 15:22:10 +0100

    Mini Update questionnaire.h

--- questionnaire.h
+++ questionnaire.h
@@ -8,7 +8,6 @@ using namespace std;
 
 class questionnaire {
     public:
-        virtual ~questionnaire() =default;
         questionnaire(const string &titre);
         vector<unique_ptr<question>> questionsFausses() const;
         int nbQuestionsRepondus()   const;
@@ -17,13 +16,14 @@ class questionnaire {
         int score() const;
         void changeScore(int nscore); 
         void sauvegarder(const string& nomFichier) const;
-        virtual void chargement(const string& nomFichier);
+        void chargement(const string& nomFichier);
     private:
-        void ajouteQuestion(const unique_ptr<question> q);
+        void ajouteQuestion(unique_ptr<question> q);
         vector<unique_ptr<question>>d_questions;
         int d_score;
         string d_titre;
 };
+
 
 
 

e2059082014fb4c10efaaf7461c2b70a01f4a23d
= <=>
2025-12-17 10:53:47 +0100

    qcm estJuste bug fixé

--- questionChoixMultiples.cpp
+++ questionChoixMultiples.cpp
@@ -18,20 +18,27 @@ void questionChoixMultiples::enleverReponse(int i)
     d_reponses[i].changeSelection(false);
 }
 
-std::string questionChoixMultiples::bonneReponse()  const
+std::string questionChoixMultiples::bonneReponse() const
 {
-    int i = 0;
-    while(i < d_reponses.size() && d_reponses[i].estJuste())
-        i++;
-    return d_reponses[i].nom();
+    for(const auto& r : d_reponses)
+    {
+        if(r.estJuste())
+            return r.nom();
+    }
+    return "";
 }
-bool questionChoixMultiples::verifierReponse(string saisie) const
+
+
+bool questionChoixMultiples::verifierReponse(string /* saisie */) const
 {
-    int i = 0;
-    while(i < d_reponses.size() && d_reponses[i].estJuste() && d_reponses[i].estSelectionne())
-        i++;
-    return i == d_reponses.size();
+    for(const auto& r : d_reponses)
+    {
+        if(r.estJuste() != r.estSelectionne())
+            return false;
+    }
+    return true;
 }
+
 
 void questionChoixMultiples::ecrireQuestion(ostream& os) const {
     os << "Q" << endl;

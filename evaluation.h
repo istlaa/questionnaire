066c085d4e6144876e857c1aa1b513cc3763dc89
Yemine0310 <yemsdrums@gmail.com>
2025-12-31 00:10:34 +0100

    Refactor class mère evaluation

--- evaluation.h
+++ evaluation.h
@@ -2,29 +2,31 @@
 #define EVALUATION_H
 
 #include "questionnaire.h"
-#include <vector>
-#include <memory>
 #include <string>
 
-class Evaluation {
+class evaluation {
 public:
-    virtual ~Evaluation() = default;
-    virtual bool aUneQuestion() const = 0;
+    explicit evaluation(questionnaire& q);
+    virtual ~evaluation() = default;
+
+    virtual void commencer() = 0;
+    virtual bool aEncoreDesQuestions() const = 0;
     virtual question& questionCourante() = 0;
-    virtual vector<question*> questionsFausses() const = 0;
-    virtual void donneReponse(const std::string& rep) = 0;
+
+    virtual void repondre(const std::string& saisie) = 0;
     virtual bool peutAfficherBonneReponse() const = 0;
     virtual void questionSuivante() = 0;
-    virtual int score() const = 0;
-    virtual void augmenteScore(int nscore) = 0;
-    virtual bool estFini() const = 0;
-    virtual int nbQuestionsRepondus()   const = 0;
-protected:
 
-    std::vector<std::unique_ptr<question>> d_questions;
-    std::size_t d_indiceCourant = 0;
-    int d_score = 0;
+    virtual int nombreQuestions() const = 0;
+    virtual int nombreEssais() const = 0;
+    virtual int nombreBonnesReponses() const = 0;
+
+protected:
+    questionnaire& questionnaireRef();
+    const questionnaire& questionnaireRef() const;
+
+private:
+    questionnaire& d_questionnaire;
 };
 
 #endif
-
